f36cf96eab2909265fc5ff3642714e73c8698994
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2022-10-04 13:59:49 +0300

    edited controller

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,14 +1,17 @@
 package controllers
 
 import (
+	"github.com/labstack/echo/v4"
 	"net/http"
 	"setup/database"
-
-	"github.com/labstack/echo/v4"
+	models "setup/models"
 )
 
 func GetUsers(c echo.Context) error {
+	var users []models.User
 	db := database.DBManager()
-	db.Find("")
-	return c.JSON(http.StatusOK, "")
+	db.Find(&users)
+	return c.JSON(http.StatusOK, map[string][]models.User{
+		"users": users,
+	})
 }
