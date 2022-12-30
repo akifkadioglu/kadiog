2d9d15dd6318540672cabe451d2b18e81805e098
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2022-12-31 00:08:46 +0300

    added profile

--- routes/init.go
+++ routes/init.go
@@ -3,21 +3,24 @@ package routes
 import (
 	"setup/adapter"
 	"setup/helpers"
-
+	_ "net/http/pprof"
+	"net/http"
+	
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	
 )
 
-var E = echo.New()
 
 func Init() {
-
+	var E = echo.New()
 	Network := E.Group("", adapter.ConsoleAdapter)
 	Network.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{"*"},
 	}))
 	Web(Network)
+	E.GET("/debug/*", echo.WrapHandler(http.DefaultServeMux))
 	port := helpers.GoDotEnvVariable("HOST") + ":" + helpers.GoDotEnvVariable("PORT")
 	if port == ":" {
 		port = "0.0.0.0:9000" // Default port if not specified
